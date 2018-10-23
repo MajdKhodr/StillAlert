da83fb667dd5e1f44f5c05eda7e4768bf2b1ec6f
Alexi10 <alexisharara@gmail.com>
2018-10-23 18:12:55 -0400

    Creating UI for starting page

--- Android/app/src/main/java/com/stillalert/gng2101/stillalert/MainActivity.java
+++ Android/app/src/main/java/com/stillalert/gng2101/stillalert/MainActivity.java
@@ -1,7 +1,9 @@
 package com.stillalert.gng2101.stillalert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void stopActivity(View view){
+        Intent intent = new Intent(this, StopActivity.class);
+        startActivity(intent);
     }
 }
---
+++ Android/app/src/main/java/com/stillalert/gng2101/stillalert/StopActivity.java
@@ -0,0 +1,13 @@
+package com.stillalert.gng2101.stillalert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop);
+    }
+}
