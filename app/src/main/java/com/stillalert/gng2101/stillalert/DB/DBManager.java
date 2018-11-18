8dd433a678095b8d5bd592b05570692a0c165c14
AliKhanafer7 <ali.h.khanafer@gmail.com>
2018-11-18 14:44:12 -0500

    Added DB class (nothing yet)

---
+++ app/src/main/java/com/stillalert/gng2101/stillalert/DB/DBManager.java
@@ -0,0 +1,32 @@
+package com.stillalert.gng2101.stillalert.DB;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class DBManager {
+
+
+}
