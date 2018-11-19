585ad6c29005e1f249c13b4122fc7ff9cbb470d5
MajdKhodr <majd_khodr@hotmail.com>
2018-11-19 15:36:34 -0500

    buttons

--- app/src/main/java/com/stillalert/gng2101/stillalert/SettingsActivity.java
+++ app/src/main/java/com/stillalert/gng2101/stillalert/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.stillalert.gng2101.stillalert;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,10 +10,14 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    public Switch mSoundSwitch;
+    public Switch mVibrationSwitch;
     public EditText mIdleTime;
+    public boolean vibrate;
+    public boolean sound;
     public String time;
 
     private static final String TAG = "SettingsActivity";
@@ -22,6 +28,8 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        mSoundSwitch = findViewById(R.id.switch_sound);
+        mSoundSwitch = findViewById(R.id.switch_vibration);
         mIdleTime = findViewById(R.id.time_editText);
 
         mIdleTime.addTextChangedListener(new TextWatcher() {
@@ -44,8 +52,15 @@ public class SettingsActivity extends AppCompatActivity {
 
 
     public void save(View view) {
+        sound = mSoundSwitch.isChecked();
+        vibrate = mVibrationSwitch.isChecked();
+
+
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("Sound", time);
+        intent.putExtra("Vibrate", time);
         intent.putExtra("Time", time);
+
         startActivity(intent);
 
         Log.d(TAG, "Settings activity Time = " + time);
