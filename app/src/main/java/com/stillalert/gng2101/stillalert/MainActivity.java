332834cc93c3a7c64a42c4bdbbcee78419d74b3a
taylorleb <taylorleb@hotmail.com>
2018-11-19 16:06:13 -0500

    Add files via upload

--- app/src/main/java/com/stillalert/gng2101/stillalert/MainActivity.java
+++ app/src/main/java/com/stillalert/gng2101/stillalert/MainActivity.java
@@ -1,11 +1,19 @@
 package com.stillalert.gng2101.stillalert;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton mButton;
     private TextView mTextView;
+    private TextView mDisplayTime;
     private ImageView mSetting;
+    private Sensor mSensor;
+    private SensorManager mSensorManager;
+    private float tempX, tempY, tempZ;
+    private String flag;
+    private int counter;
+    private int time = 0;
 
     private static final String TAG = "MainActivity";
 
@@ -23,18 +38,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        flag = "OFF";
+
         mTextView = findViewById(R.id.button_start);
 
         mSetting = findViewById(R.id.imageview_setting);
 
         mButton = findViewById(R.id.floatingActionButton_start);
 
+        mDisplayTime = findViewById(R.id.textview_time);
+
+        Log.d(TAG, "value is " + getIntent().getSerializableExtra("Time"));
+
+        if (getIntent().getSerializableExtra("Time") != null) {
+            time = (int) getIntent().getSerializableExtra("Time");
+        }
+
+        Log.d(TAG, "MainActivity Time = " + time);
+
+        mDisplayTime.setText(Integer.toString(time));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        counter = 0;
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,11 +73,13 @@ public class MainActivity extends AppCompatActivity {
                         view.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ff0000")));
                         mTextView.setText("Stop");
                         mSetting.setVisibility(View.INVISIBLE);
+                        flag = "ON";
 
                     } else {
                         view.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#008000")));
                         mTextView.setText("Start");
                         mSetting.setVisibility(View.VISIBLE);
+                        flag = "OFF";
 
 
                     }
